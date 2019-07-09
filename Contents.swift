c8ae82cfb4778850cede5ae24f7f7b3b6b37c115
gothhouse <jamiedang1349@gmail.com>
2019-07-09 14:25:44 -0700

    second commit

--- Contents.swift
+++ Contents.swift
@@ -22,15 +22,43 @@ import UIKit
 
 class Scholar {
     //intentions: we want to create a class that serves as a blueprint for each individual scholar. Each scholar will have a different name, age, grade, location, etc.
+    //this is your blueprint
     var grade = 10
     var studying = "Swift"
-    var name = " " //default this to empty string!
+    var name = "Default"
 
-    init(scholarName : String) {
+    //the init function will run with EVERY class, meaning that we put properties there that every single object needs to have
+    //we will take the input and replace it with the properties above ^^
+    
+    init(scholarName : String, scholarGrade : Int, scholarStudying : String){
         name = scholarName
+        grade = scholarGrade
+        studying = scholarStudying
+    }
+    
+    //here is an action that I've done with code
+    func writeCode() {
+        print("\(name) is busy writing code at KWK Portland!")
     }
 }
 
+//You create your objects OUTSIDE of your class! You specialize each blueprint
+var nailahScholar = Scholar(scholarName : "Nailiah", scholarGrade : 10, scholarStudying: "Swift")
+var mayaScholar = Scholar(scholarName : "Maya", scholarGrade: 10, scholarStudying: "Swift")
+var fionaScholar = Scholar(scholarName : "Fiona", scholarGrade: 9, scholarStudying: "Swift")
 
-var nailahScholar = Scholar(scholarName : "Nailiah")
 print(nailahScholar.name)
+print(mayaScholar.name)
+print(fionaScholar.name)
+print(fionaScholar.grade)
+print(fionaScholar.studying)
+fionaScholar.writeCode()
+
+////test run w printall
+//func printall (scholar : String) {
+//    print(\(scholar).name)
+//    print(\(scholar).grade)
+//    print(\(scholar).studying)
+//}
+//
+//printall(scholar: "nailahScholar")
